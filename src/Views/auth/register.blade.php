721327bd31b3e5b7d6770fefc3f98b3d49ae676f
landjea <jean-guy@landriault.com>
2015-12-21 12:57:06 -0500

    Added Laravel's default auth views for convenience

---
+++ src/Views/auth/register.blade.php
@@ -0,0 +1,58 @@
+@extends(config('watchtower.views.layouts.master'))
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Name</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                                {!! $errors->first('name', '<div class="text-danger">:message</div>') !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">E-Mail Address</label>
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                {!! $errors->first('email', '<div class="text-danger">:message</div>') !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Password</label>
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+                                {!! $errors->first('password', '<div class="text-danger">:message</div>') !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Confirm Password</label>
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password_confirmation">
+                                {!! $errors->first('password_confirmation', '<div class="text-danger">:message</div>') !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
